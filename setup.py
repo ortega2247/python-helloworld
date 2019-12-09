b56fcfccd93169e98769bfdd042bb8cefb0b3d26
ortega2247 <oo.ortega2247@gmail.com>
2019-12-09 15:53:21 -0600

    update setup

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import sys
 from setuptools import setup
 
 setup(
-    name = "helloworld",        # what you want to call the archive/egg
-    version = "0.1",
+    name = "test_package_ortega2247",        # what you want to call the archive/egg
+    version = "0.2",
     packages=["helloworld"],    # top-level python modules you can import like
                                 #   'import foo'
     dependency_links = [],      # custom links to a specific project
